fc1c231b97fa691c6ca15df6c30a84490e6c81ee
oracle <oracle@myoci.site>
2022-04-14 13:52:45 +0800

    update

--- README.md
+++ README.md
@@ -25,3 +25,14 @@ kubectl apply -f deploy/
 curl YOUR_IP:8081/http
 curl YOUR_IP:8081/grpc
 ```
+
+### use http2
+
+```aidl
+openssl req -x509 -out mylocalserver.com.crt -keyout mylocalserver.com.key \
+  -newkey rsa:4096 -nodes -sha256 \
+  -subj '/CN=mylocalserver' -extensions EXT -config <( \
+   printf "[dn]\nCN=mylocalserver\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:mylocalserver.com\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
+openssl pkcs12 -export -in mylocalserver.com.crt -inkey mylocalserver.com.key -name mylocalserver.com -out mylocalserver.com.p12
+# Password
+```
--- src/main/java/com/example/microservice/controller/MainController.java
+++ src/main/java/com/example/microservice/controller/MainController.java
@@ -3,6 +3,7 @@ package com.example.microservice.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -29,8 +30,28 @@ public class MainController {
         }
     }
 
+    @GetMapping("/http2")
+    public String http2() {
+
+        try {
+            RestTemplate restTemplate = new RestTemplate(new OkHttp3ClientHttpRequestFactory());
+
+            ResponseEntity<String> response
+                    = restTemplate.getForEntity(httpServerUrl + "/ms2http2", String.class);
+            return response.getBody().toString().replace("HTTP_SERVER_URL", httpServerUrl);
+        } catch (Exception e) {
+            log.error(e.toString());
+            return "[Failed]Microservice 1 Invoke Microservice 2 [" + httpServerUrl + "] via HTTP2 Failed.\n";
+        }
+    }
+
     @GetMapping("/ms2")
     public String ms2() {
         return "[Successed]Microservice 1 Invoke Microservice 2 [HTTP_SERVER_URL] via HTTP Successed.";
+    }
+
+    @GetMapping("/ms2http2")
+    public String ms2http2() {
+        return "[Successed]Microservice 1 Invoke Microservice 2 [HTTP_SERVER_URL] via HTTP2 Successed.";
     }
 }
